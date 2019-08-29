54e722fce30b0186e511c255ab9a5c72a2bd4b80
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-29 01:07:58 -0300

    styling the to-do list

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="tasks">
-      <template>
+  <div class="task-grid">
+      <template v-if="tasks.length">
           <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
       </template>
+      <p class="no-task" v-else>Sua vida está em dia :)</p>
   </div>
 </template>
 
@@ -17,5 +18,18 @@ export default {
 </script>
 
 <style>
+    .task-grid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+    
+    .task-grid .task {
+        margin: 10px;
+    }
 
+    .no-task {
+        color: #AAA;
+        font-size: 1.7rem;
+    }
 </style>
