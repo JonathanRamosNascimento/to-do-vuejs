57abd28c2ce70092c5e6d6d33133ccebcab09b85
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-09-04 00:55:15 -0300

    creating the method of deleting tasks

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="task-grid">
-      <template v-if="tasks.length">
-          <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
-      </template>
-      <p class="no-task" v-else>Sua vida está em dia :)</p>
+    <template v-if="tasks.length">
+      <Task v-for="(task, i) in tasks" :key="task.name" @taskDeleted="$emit('taskDeleted', i)" :task="task"></Task>
+    </template>
+    <p class="no-task" v-else>Sua vida está em dia :)</p>
   </div>
 </template>
 
 <script>
-import Task from './Task.vue'
+import Task from "./Task.vue";
 export default {
-    components: { Task },
-    props: {
-        tasks: {type: Array, required: true}
-    }
-}
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true }
+  }
+};
 </script>
 
 <style>
-    .task-grid {
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
-    }
-    
-    .task-grid .task {
-        margin: 10px;
-    }
+.task-grid {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
 
-    .no-task {
-        color: #AAA;
-        font-size: 1.7rem;
-    }
+.task-grid .task {
+  margin: 10px;
+}
+
+.no-task {
+  color: #aaa;
+  font-size: 1.7rem;
+}
 </style>
